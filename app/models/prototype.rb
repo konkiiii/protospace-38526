920e02d868369c9c27c4bc5bb2cf66ebe74ecf75
konkiiii <kintaro0827@icloud.com>
2022-12-29 17:39:03 +0900

    投稿機能の実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,9 +1,12 @@
 class Prototype < ApplicationRecord
-  validates :text, presence:true
-  validates :string, presence:true
-  validates :image, presence:true
+  with_options presence:true do
+    validates :title
+    validates :catch_copy
+    validates :concept
+    validates :image
+  end
   # 念の為、空欄時に保存できないバリデーション設定→いらない場合は削除可
-  belomgs_to :user
+  belongs_to :user
   # has_many :comments
   # ※commentsのアソシエーション作成時にコメントアウト外す
   has_one_attached :image
